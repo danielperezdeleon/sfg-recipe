4b0d93bf74cc089cab434649f3d5fd4279136c02
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-19 17:34:58 -0700

    Adding Ingredients and its relationship to Recipes with JPA annotations

---
+++ src/main/java/com/immutableant/sfgrecipe/domain/Ingredient.java
@@ -0,0 +1,42 @@
+package com.immutableant.sfgrecipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    //private UnitOfMeasure uom;
+    @ManyToOne
+    private Recipe recipe;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
