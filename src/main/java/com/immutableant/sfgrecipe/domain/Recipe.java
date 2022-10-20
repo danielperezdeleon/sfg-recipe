3e24dcdf2627f8d13e68e22264be27678467a6b2
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-10-20 11:47:35 -0700

    Added Category and its relationship to Recipe

--- src/main/java/com/immutableant/sfgrecipe/domain/Recipe.java
+++ src/main/java/com/immutableant/sfgrecipe/domain/Recipe.java
@@ -18,7 +18,8 @@ public class Recipe {
   private String url;
   private String directions;
 
-  @Enumerated(value = EnumType.STRING) private Difficulty difficulty;
+  @Enumerated(value = EnumType.STRING)
+  private Difficulty difficulty;
   // todo add
   // private Difficulty difficulty;
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -28,6 +29,13 @@ public class Recipe {
 
   @OneToOne(cascade = CascadeType.ALL)
   private Notes notes;
+
+  @ManyToMany
+  @JoinTable(
+      name = "recipe_category",
+      joinColumns = @JoinColumn(name = "recipe_id"),
+      inverseJoinColumns = @JoinColumn(name = "category_id"))
+  private Set<Category> categories;
 
   public Difficulty getDifficulty() {
     return difficulty;
@@ -115,5 +123,21 @@ public class Recipe {
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+  }
+
+  public Set<Ingredient> getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(Set<Ingredient> ingredients) {
+    this.ingredients = ingredients;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
+  }
+
+  public void setCategories(Set<Category> categories) {
+    this.categories = categories;
   }
 }
